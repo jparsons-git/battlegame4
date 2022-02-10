07748919bab2143dcfe90ea946f3327857d8c3bb
Jane Parsons <jane.parsons@waitrose.co.uk>
2022-02-10 15:35:58 +0000

    Fleshed out our feature tests

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,8 +1,17 @@
 
 feature 'Testing entry and display of players names' do
-    scenario 'Can run app and check page content' do
+    scenario 'Test basic content when loading screen' do
       visit('/')
       expect(page).to have_content 'Enter Player 1:'
       expect(page).to have_content 'Enter Player 2:'
+    end
+
+    scenario 'Test basic content when loading screen' do
+      visit('/')
+      fill_in 'player1', with: 'Trushna'
+      fill_in 'player2', with: 'Jane'
+      click_on 'Submit'
+      expect(page).to have_content 'Player 1 is Trushna'
+      expect(page).to have_content 'Jane'
     end
   end
