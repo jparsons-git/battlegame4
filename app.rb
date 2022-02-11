68c91e8a0e4d59b412f5f8a8fb628075060e845b
Jane Parsons <jane.parsons@waitrose.co.uk>
2022-02-11 13:48:39 +0000

    Store the actual Player class as a global attribrute,, rather than just the name

--- app.rb
+++ app.rb
@@ -18,28 +18,33 @@ class Battle < Sinatra::Base
   post '/names' do
     p params
     # Jane and Trushna's way
-    $player1 = params[:player1]
-    $player2 = params[:player2]
+    # $player1 = params[:player1]
+    # $player2 = params[:player2]
     # The walkthrough way
-    # $player1 = Player.new(params[:player1])
-    # $player2 = Player.new(params[:player2])
+    $player1 = Player.new(params[:player1])
+    $player2 = Player.new(params[:player2])
     redirect '/play'
   end
 
   get '/play' do
-    # player1 = session[:player1]
-    # player2 = session[:player2]
-    @player1 = $player1
-    @player2 = $player2
+    # Jane and Trushna's way
+    # @player1 = $player1
+    # @player2 = $player2
+    # The walkthrough way
+    @player1 = $player1.name
+    @player2 = $player2.name
     erb :play
   end
 
   post '/attack' do 
-    @player1 = $player1
-    @player2 = $player2
+    # Jane and Trushna's way
+    # @player1 = $player1
+    # @player2 = $player2
+    # The walkthrough way
+    @player1 = $player1.name
+    @player2 = $player2.name
     erb :attack
   end
-
 
   # # Start the server if this file is executed directly (do not change the line below)
   run! if app_file == $0
